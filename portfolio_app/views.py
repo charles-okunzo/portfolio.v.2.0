cd8db914a2bb491e6f2e01c064437c98c3783767
Guyo <you@example.com>
2022-06-22 08:48:59 +0300

    feature --add index file

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title='Charles P. Okunzo | Welcome to my Portfolio'
+    context = {
+        'title':title
+    }
+    return render(request, 'portfolio_app/index.html', context)
