8f33c894cef9e3b349bf96568273be4b0dd39863
Guyo <you@example.com>
2022-06-23 22:57:16 +0300

    style --modify hire and feedback forms

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,22 @@
-if ($(".typed-word").length == 1) {
+$(()=>{
+    if ($(".typed-word").length == 1) {
 
-    var typed = new Typed(".typed-word", {
-        strings: ['Full-stack Web Developer', 'Web Designer', 'UI/UX Designer'],
-        typeSpeed: 50,
-        loop: true,
-        backDelay: 900,
-        backSpeed: 30,
-    });
-}
+        var typed = new Typed(".typed-word", {
+            strings: ['Full-stack Web Developer', 'Web Designer', 'UI/UX Designer'],
+            typeSpeed: 50,
+            loop: true,
+            backDelay: 900,
+            backSpeed: 30,
+        });
+    }
+    
+    $('.hire-me').submit(()=>{
+        let name = $('#hirer-name').val()
+        alert(`Hello ${name}, Your submission has been received succeessfully. Thank you for contacting Dev Okunzo`)
+    })
+
+    $('.feedback').submit(()=>{
+        let name = $('#fb-name').val()
+        alert(`Hello ${name}, Your feedback has been received succeessfully. Thank you for contacting Dev Okunzo`)
+    })
+})
