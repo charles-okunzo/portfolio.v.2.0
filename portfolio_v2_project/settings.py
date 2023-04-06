09c0f29198d17d283683ff3c102e627c0c02f94c
charles-okunzo <okunzopeter01@gmail.com>
2023-04-06 15:55:42 +0300

    install new dependensies

--- portfolio_v2_project/settings.py
+++ portfolio_v2_project/settings.py
@@ -78,10 +78,8 @@ WSGI_APPLICATION = 'portfolio_v2_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
