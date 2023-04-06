80aeb20195f317255b09664b74dc5431a5f555b0
charles-okunzo <okunzopeter01@gmail.com>
2023-04-06 16:02:21 +0300

    install new dependensies

--- portfolio_v2_project/settings.py
+++ portfolio_v2_project/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'portfolio_v2_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 }
 
